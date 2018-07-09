1899d7878221c1f81fb8c3000e5adcecf172aa77
Nan Liang <nan_liang@harvard.edu>
2018-07-09 05:07:29 +0000

    places inputted, places searchable, cach previous search results and queries

--- application.py
+++ application.py
@@ -23,10 +23,92 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    # initialize user id
-    if(not 'user_id' in session):
-        session['user_id'] = 0;
-    return render_template('index.html', user_id=session['user_id']);
+    # initialize global varaiables
+    if 'user_id' not in session:
+        session['user_id'] = 0
+    if 'results' not in session:
+        session['results'] = ['']
+    if 'index' not in session:
+        session['index'] = 0
+    if 'query' not in session:
+        session['query'] = ('', '', '')
+    # stri = "no sql"
+
+    # get input data
+    city = request.args.get('city', '')
+    state = request.args.get('state', '')
+    zipreal = request.args.get('zipcode', -1)
+    if zipreal == '':
+        zipcode = int(-1)
+    else:
+        zipcode = int(zipreal)
+
+    # if no input, use last recorded query
+    if (city, state, zipcode) == ('', '', -1):
+        old_query = session['query']
+        city = old_query[0]
+        state = old_query[1]
+        if old_query[2] != '':
+            zipcode = old_query[2]
+
+    # if current query is different from last query and not empty
+    # update query and get new results
+    elif (city, state, zipreal) != session['query']:
+        session['query'] = (city, state, zipreal)
+        sql0 = "SELECT city, state, zipcode, population, lat, long FROM places "
+        # query the zipcode (can't wildcard zipcode b.c it is an integer primary key)
+        if int(zipcode) != -1:
+            # stri = sql0 + "WHERE zipcode= %d" % (int(zipcode))
+            temp = db.execute(sql0 + "WHERE zipcode= %d" % (int(zipcode))).fetchone()
+            subset = [temp]
+
+            if temp is not None:
+                # found location, return to mainpage
+                session['results'] = subset
+                return render_template('index.html',
+                            user_id=session['user_id'],
+                            city=city,
+                            state=state,
+                            zipcode=zipreal,
+                            # stri=stri,
+                            results=subset);
+        # if zipcode fails, try %city% and state, sort by zipcode
+        if city == '' and state == '' :
+            sql1 = ''
+        elif city == '':
+            sql1 = "WHERE lower(state) LIKE lower(:state)"
+        elif state == '':
+            sql1 = "WHERE lower(city) LIKE lower(:like_city)"
+        else:
+            sql1 = "WHERE lower(city) LIKE lower(:like_city) AND state=:state"
+        like_city = "%" + city + "%"
+        sql2 = "SELECT * FROM ("
+        sql3 = ") temp ORDER BY temp.zipcode"
+        arg = {'like_city':like_city, 'state':state}
+        temp = db.execute(sql2+sql0+sql1+sql3, arg).fetchall()
+        # stri=sql2+sql0+sql1+sql3, arg
+        session['results'] = temp
+
+    # get and parse results (should not be None)
+    results = session['results']
+    if results == None:
+        return redirect(url_for('error',
+                                ret='index',
+                                return_message="Go back to the main page!"))
+    elif (results == ['']) or (results == []):
+        subset = results
+    else:
+        index = session['index']
+        results[index*10 : (index+1)*10]
+        subset = results
+
+    return render_template('index.html',
+                            user_id=session['user_id'],
+                            city=city,
+                            state=state,
+                            zipcode=zipreal,
+                            # stri=stri,
+                            results=subset);
 
 @app.route("/logout", methods=["POST","GET"])
 def logout():
@@ -70,7 +152,7 @@ def login():
             return_message="Go back to the main page!"))
 
     # search for user, then check hashed pw
-    row = db.execute("SELECT DISTINCT user_id, password FROM users WHERE username = :username",
+    row = db.execute("SELECT DISTINCT user_id, password FROM users WHERE lower(username) = lower(:username)",
                     {"username": username_form}).fetchone()
     if row is not None:
         m = hashlib.md5()
@@ -124,7 +206,7 @@ def registration():
             error="Please enter both username and password!"))
 
     # check username available
-    row = db.execute("SELECT DISTINCT user_id FROM users WHERE username = :username",
+    row = db.execute("SELECT DISTINCT user_id FROM users WHERE lower(username) = lower(:username)",
                     {"username": username_form}).fetchone()
     if row is not None:
         return redirect(url_for('register',
@@ -140,7 +222,7 @@ def registration():
     db.commit()
 
     # log the user in
-    uid = db.execute("SELECT DISTINCT user_id FROM users WHERE username=:user",
+    uid = db.execute("SELECT DISTINCT user_id FROM users WHERE lower(username) = lower(:user)",
                     {"user": username_form}).fetchone()
     session['user_id'] = uid[0]
     return redirect(url_for('index'))
